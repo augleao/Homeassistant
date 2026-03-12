549e75052ad575d1e117ca35b779f626cacbd530
augleao <augleao@gmail.com>
2026-03-11 21:34:30 -0300

    Remove client_secret and secure_key from addon configuration

--- onedrive_backup/start.sh
+++ onedrive_backup/start.sh
@@ -10,12 +10,6 @@ o=json.load(open('/data/options.json'))
 print(o.get('client_id','') or '')
 PY
 )
-  CLIENT_SECRET=$(python - <<'PY'
-import json,sys
-o=json.load(open('/data/options.json'))
-print(o.get('client_secret','') or '')
-PY
-)
   TENANT_ID=$(python - <<'PY'
 import json,sys
 o=json.load(open('/data/options.json'))
@@ -28,18 +22,10 @@ o=json.load(open('/data/options.json'))
 print(o.get('backup_path','/backup') or '/backup')
 PY
 )
-  SECURE_KEY=$(python - <<'PY'
-import json,sys
-o=json.load(open('/data/options.json'))
-print(o.get('secure_key','') or '')
-PY
-)
   # export to environment for the python app
   export CLIENT_ID
-  export CLIENT_SECRET
   export TENANT_ID
   export BACKUP_PATH
-  export SECURE_KEY
 fi
 
 python main.py
